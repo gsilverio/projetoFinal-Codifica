e4b4723df872c8e1296009d2c368a76f3a8ad3df
gsilverio <guilhermegsilverio@gmail.com>
2024-09-19 17:30:04 -0300

    Setting up bootstrap

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Rotas from "./Rotas";
+import "./App.css";
+import "./assets/custom/custom.scss";
+import * as bootstrap from "bootstrap";
+
+function App() {
+  return (
+    <>
+      <Rotas />
+    </>
+  );
+}
+
+export default App;
